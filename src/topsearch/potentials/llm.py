9f2d2506331c7f8daa2ac2da460533e9d0e1533b
Vlad Carare <cararevlad96@gmail.com>
2024-07-04 15:42:12 +0100

    added llm to potentials - only for batchsize == size_data so far

---
+++ src/topsearch/potentials/llm.py
@@ -0,0 +1,189 @@
+""" Module that contains the class for evaluating the loss and gradients
+    of a Large Language Model (LLM) via the Huggingface Transformers and Database libraries"""
+
+
+""" Example of use:
+import torch 
+
+try: 
+    import intel_extension_for_pytorch as ipex
+    print('Found IPEX package for running models on Intel GPUs.')
+except:
+    pass
+
+
+# Load data
+from datasets import load_dataset
+imdb = load_dataset("imdb")
+
+# Create a smaller training dataset for faster training times
+small_train_dataset = imdb["train"].shuffle(seed=42).select([i for i in list(range(16))])
+small_test_dataset = imdb["test"].shuffle(seed=42).select([i for i in list(range(10))])
+
+# Set DistilBERT tokenizer
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("gpt2-large")
+
+
+# Define DistilBERT as our base model:
+from transformers import AutoModelForSequenceClassification
+model = AutoModelForSequenceClassification.from_pretrained("gpt2-large", num_labels=2)
+
+model.to('xpu')
+model = ipex.optimize(model)
+for param in model.base_model.parameters():
+    param.requires_grad = False
+
+
+
+if tokenizer.pad_token is None:
+    print(f'Inserting pad token')
+    tokenizer.add_special_tokens({'pad_token': '<|padding|>'})
+    model.resize_token_embeddings(len(tokenizer))
+
+model.config.pad_token_id = tokenizer.pad_token_id
+model.config.pad_token_id
+
+model.save_pretrained("/home/vc381/rds/hpc-work/09052024-distillBertFineTunningSentiment/distil/test", from_pt=True) 
+model_new = AutoModelForSequenceClassification.from_pretrained("/home/vc381/rds/hpc-work/09052024-distillBertFineTunningSentiment/distil/test")
+model_new.to('xpu')
+model_new = ipex.optimize(model_new)
+for param in model_new.base_model.parameters():
+    param.requires_grad = False
+
+# Prepare the text inputs for the model
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
+tokenized_train = small_train_dataset.map(preprocess_function, batched=True)
+tokenized_test = small_test_dataset.map(preprocess_function, batched=True)
+
+
+# Use data_collector to convert our samples to PyTorch tensors and concatenate them with the correct amount of padding
+from transformers import DataCollatorWithPadding
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+
+# Define a new Trainer with all the objects we constructed so far
+from transformers import TrainingArguments, Trainer
+
+repo_name = "test"
+
+
+training_args = TrainingArguments(
+    output_dir=repo_name,
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=0,
+    weight_decay=0.01,
+    push_to_hub=True,
+    use_ipex=True,
+)
+
+trainer_just_head = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_train,
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+)
+
+trainer_just_head.train()
+"""
+import numpy as np
+from nptyping import NDArray
+from ase import Atoms
+from .potential import Potential
+import transformers
+import torch 
+
+class LLM(Potential):
+    """
+    Description
+    ---------------
+    Evaluate the loss and gradients of a Huggingface model, wrapped inside a Trainer, as in the example.
+
+    Attributes
+    ---------------
+    trainer : transformer.trainer.Trainer
+        A Huggingface wrapper for Pytorch models, datasets and arguments (such as batch size).
+    """
+
+    def __init__(self, trainer: transformers.trainer.Trainer) -> None:
+        self.n_params = trainer.get_num_trainable_parameters()
+        if self.n_params > 1e4:
+            raise ValueError(f"Number of trainable parameters is {self.n_params}. We can only accept values smaller than 10,000.\n Make sure you use\n\"for param in model.base_model.parameters():\n param.requires_grad = False\"\n or select a model with a smaller head.")
+        self.trainer = trainer
+        self.model = trainer.model
+        self.trainable_layers_names_and_sizes = [(name,param.size()) for (name,param) in self.model.named_parameters() if param.requires_grad]
+        self.inputs = trainer.get_train_dataloader()
+        if len(self.inputs)!=1:
+            raise ValueError(f"At the moment we cannot evaluate mini-batches. Make sure the size of the training batch matches the size of data.")
+        for _ in self.inputs:
+            self.inputs = _ # trainer.get_train_dataloader() returns an iterator whose item I can only access using this trick
+
+    def function(self, position: NDArray) -> float:
+        """ Compute the loss value at a specific configuration of weights """
+        self.set_new_weights(position)
+        loss = self.trainer.prediction_step(self.model,self.inputs,prediction_loss_only=True)
+        return loss[0].item()
+
+    def function_gradient(self, position: NDArray) -> tuple:
+        """ Compute the loss and gradient at a specific configuration of weights """
+        self.set_new_weights(position) # modifies the weights of the model according to the position vector
+        inputs = self.trainer._prepare_inputs(self.inputs)
+        self.model.eval()
+        self.model.zero_grad()
+        loss = self.trainer.compute_loss(self.model, inputs)
+        self.trainer.accelerator.backward(loss)
+        loss = loss.detach() / self.trainer.args.gradient_accumulation_steps
+        gradients = []
+        # get gradients
+        for layer_name, _ in self.trainable_layers_names_and_sizes:
+            layer = self.find_layer(self.model,layer_name) # get layer by name 
+            gradients.append(layer.grad.flatten().tolist())
+        gradients = np.concatenate([gradients])
+        return loss.item(), gradients.flatten()
+
+    def gradient(self, position: NDArray) -> NDArray:
+        """ Compute the loss and gradient at a specific configuration of weights """
+        self.set_new_weights(position) # modifies the weights of the model according to the position vector
+        inputs = self.trainer._prepare_inputs(self.inputs)
+        self.model.eval()
+        self.model.zero_grad()
+        loss = self.trainer.compute_loss(self.model, inputs)
+        self.trainer.accelerator.backward(loss)
+        loss = loss.detach() / self.trainer.args.gradient_accumulation_steps
+        gradients = []
+        # get gradients
+        for layer_name, _ in self.trainable_layers_names_and_sizes:
+            layer = self.find_layer(self.model,layer_name) # get layer by name 
+            gradients.append(layer.grad.flatten().tolist())
+        gradients = np.concatenate([gradients])
+        return gradients.flatten()
+
+    def set_new_weights(self, position: NDArray) -> None:
+        """ Compute the loss value at a specific configuration of weights """
+        if len(position) != self.n_params:
+            raise ValueError(f"Number of elements of the position array {len(position)} needs to be equal to the number of trainable parameters of the network {self.n_params}.")
+        # resize position parameters to the sizes of the trainable layers, and reset the corresponding parameters
+        position = (_ for _ in position) 
+        for layer_name, size in self.trainable_layers_names_and_sizes:
+            layer = self.find_layer(self.model,layer_name) # get layer by name 
+            num_weights = layer.numel()
+            new_weights = torch.Tensor([next(position) for _ in range(num_weights)]).reshape(size).to(self.model.device)
+            layer.data = new_weights
+
+    @staticmethod
+    def find_layer(model: torch.nn.Module, name: str) -> torch.nn.Module:
+        """ Find a model layer by name
+        """
+        sub_modules = name.split('.')
+        current_module = model
+        try:
+            for sub_module in sub_modules:
+                current_module = getattr(current_module, sub_module)
+            return current_module
+        except AttributeError:
+            return None
---
+++ tests/potentials/test_llm.py
@@ -0,0 +1,90 @@
+# import pytest
+from transformers import (
+        GPT2Config,
+        GPT2LMHeadModel,
+        TrainingArguments, 
+        Trainer
+    )
+import tempfile
+import torch 
+from topsearch.potentials import llm
+import numpy as np
+torch.use_deterministic_algorithms(True)
+torch.manual_seed(0)
+
+class RepeatDataset:
+    """Class copied from transformers/trainer_tests.py"""
+    def __init__(self, x, length=64):
+        self.x = x
+        self.length = length
+    def __len__(self):
+        return self.length
+    def __getitem__(self, i):
+        return {"input_ids": self.x, "labels": self.x}
+
+def test_llm_weight_setting():
+    # create a very small model akin to what one may encounter in production
+    config = GPT2Config(vocab_size=10, n_positions=20, n_embd=2, n_layer=1, n_head=1)
+    tiny_gpt2 = GPT2LMHeadModel(config)
+    # create random (but determistic) training set
+    x = torch.randint(0, 10, (20,))
+    train_dataset = RepeatDataset(x,length=10)
+    # the Trainer object needs a working directory, so create a temporary one
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # Trainer without inf/nan filter
+        args = TrainingArguments(
+            tmpdir, learning_rate=1e-13, per_device_train_batch_size=10, logging_steps=5, logging_nan_inf_filter=False, use_cpu=True
+        )
+        trainer = Trainer(tiny_gpt2, args, train_dataset=train_dataset)
+        # disable gradients for all but some weights - as one would do when finetuning
+        for param in trainer.model.parameters():
+            param.requires_grad = False
+        trainer.model.lm_head.weight.requires_grad = True
+        # set the model in .eval mode - this ensures some components, such as LayerNorm, do not change
+        trainer.model.eval()
+        # initialise the TopSearch interface
+        test_potential=llm.LLM(trainer)
+        # test the 3 functions 3 times, for 2 random seeds.
+        np.random.seed(123)
+        new_layer_data =  np.random.rand(20)
+        loss = test_potential.function(new_layer_data)
+        loss_gradients = test_potential.function_gradient(new_layer_data)
+        gradients = test_potential.gradient(new_layer_data)
+        np.random.seed(124)
+        new_layer_data =  np.random.rand(20)
+        loss2 = test_potential.function(new_layer_data)
+        loss_gradients2 = test_potential.function_gradient(new_layer_data)
+        gradients2 = test_potential.gradient(new_layer_data)
+        np.random.seed(123)
+        new_layer_data =  np.random.rand(20)
+        loss3 = test_potential.function(new_layer_data)
+        loss_gradients3 = test_potential.function_gradient(new_layer_data)
+        gradients3 = test_potential.gradient(new_layer_data)
+        # ensure all expected behaviours are tested
+        assert loss==loss_gradients[0]
+        assert loss2==loss_gradients2[0]
+        assert loss3==loss_gradients3[0]
+        assert loss==loss3
+        assert np.all(gradients==loss_gradients[1])
+        assert np.all(gradients2==loss_gradients2[1])
+        assert np.all(gradients3==loss_gradients3[1])
+        assert np.all(gradients==gradients3)
+        assert not np.all(gradients==gradients2)
+        expected_gradients = np.array([-0.03265307,  0.03265312, -0.05189211,  0.05189213,  0.06167838,
+        -0.06167835,  0.11434107, -0.11434112, -0.0625431 ,  0.06254306,
+         0.09921123, -0.09921129,  0.01692764, -0.01692764, -0.15870528,
+         0.15870537,  0.02924541, -0.02924554, -0.01912324,  0.01912324])
+        expected_gradients2 = np.array([-0.05143911,  0.05143914, -0.03922105,  0.03922106,  0.0161558 ,
+        -0.01615578, -0.01927603,  0.01927605, -0.03995134,  0.03995132,
+        -0.04588147,  0.04588142,  0.00554584, -0.00554584,  0.19774836,
+        -0.19774836, -0.05518682,  0.05518683,  0.0302661 , -0.03026609])
+        expected_loss = 2.333463430404663
+        expected_loss2 = 2.332834243774414
+        assert loss == expected_loss
+        assert loss2 == expected_loss2
+        # allclose because the expected_ arrays have a precision of 8 decimal places 
+        assert np.allclose(gradients,expected_gradients,atol=1e-08)
+        assert np.allclose(gradients2,expected_gradients2,atol=1e-08)
+
+
+test_llm_weight_setting()
