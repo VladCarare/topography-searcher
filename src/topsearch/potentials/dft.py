hessian units fix

src/topsearch/potentials/dft.py
@@ -122,7 +122,7 @@ class DensityFunctionalTheory(Potential):
             with contextlib.redirect_stdout(None): # silence chatty Psi4
                 hess = calc.psi4.driver.hessian(f'{method}/{basis}',
                                                 molecule=calc.molecule,).to_array()
-            hess *= Bohr**2/Hartree
+            hess *= Hartree/Bohr**2
         except Exception:
             traceback.print_exc()
             hess = np.full((position.size, position.size), np.inf, dtype=float)

src/topsearch/potentials/ml_potentials.py
@@ -157,7 +157,7 @@ class MachineLearningPotential(Potential):
             coordinates = torch.tensor(position.reshape(-1, 3),
                                        dtype=torch.float32,
                                        requires_grad=True).unsqueeze(0)
-            energy_torch = self.model((species, coordinates)).energies
+            energy_torch = self.model((species, coordinates)).energies * Hartree
             gradient_torch = torch.autograd.grad(energy_torch.sum(),
                                                  coordinates)[0]
             gradient = np.array(gradient_torch.flatten().tolist())